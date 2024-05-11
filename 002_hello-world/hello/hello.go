97d458723dc4829b4718b9b67ed267596f6a6fa1
relow <dev.antropov@gmail.com>
2024-05-11 14:37:30 +0300

    error handling

--- 002_hello-world/hello/hello.go
+++ 002_hello-world/hello/hello.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// get a greeting message and print it
-	message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
